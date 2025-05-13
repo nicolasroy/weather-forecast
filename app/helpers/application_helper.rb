4d0878a062bbeeda976389166568cb1e27d96c25
Nicolas <daitenshi.nr@gmail.com>
2025-05-13 14:59:48 -0400

    Display Fahrenheiht

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def kelvin_to_f(kelvin, precision: 0)
+    ((kelvin - 273.15) * 9 / 5 + 32).round(precision)
+  end
+
+  def kelvin_to_c(kelvin, precision: 0)
+    (kelvin - 273.15).round(precision)
+  end
 end
